f096e0de3a39afc3ff033031aea737fd6d6a16a0
acelik <celikalper84@gmail.com>
2023-02-09 16:28:22 -0500

    made parse_notes.py into a script with arguments

--- parse_notes.py
+++ parse_notes.py
@@ -1,14 +1,8 @@
 # the goal of this scritp is to select notes that contain the key lemma we are interested in
 # the action plan is as follows
-import medspacy
-import pandas as pd
-import spacy
-from medspacy.context import ConText
-from sqlalchemy import create_engine, select, Table, MetaData, or_
-from sqlalchemy.orm import Session
 
-from rules import context_rules, target_rules, to_rem
-from utils import parse_sentence
+
+import argparse as arg
 
 # search database for ed notes
 # keep a record of each metadata especially visit id
@@ -20,7 +14,24 @@ from utils import parse_sentence
 # of notes we need to parse
 # There might be more than one instance of the symptom within the note I will keep all
 
-db = create_engine("sqlite:///notes.db")  # should not hard code this
+parser=arg.ArgumentParser(description="parse ed notes and search for symptoms")
+parser.add_argument("-d", "--database", type=str, help="sqlite database path")
+parser.add_argument("-n", "--skip_n", type=int, help="skip this many notes", default=0)
+
+args=parser.parse_args()
+
+import medspacy
+import pandas as pd
+import spacy
+from medspacy.context import ConText
+from sqlalchemy import create_engine, select, Table, MetaData, or_
+from sqlalchemy.orm import Session
+from datetime import datetime
+
+from rules import context_rules, target_rules, to_rem
+from utils import parse_sentence
+
+db = create_engine("sqlite:///{}".format(args.database))  # should not hard code this
 session = Session(db)
 metadata = MetaData(db)
 metadata.reflect(bind=db)
@@ -32,6 +43,8 @@ spacy.prefer_gpu()
 # headaches. Keep in mind this is only looking for words, the idea is to "enrich" our notes for
 # what we are looking for so we don't have to parse all the notes.
 # the selection is done only in the ED notes but changing the query and the table below we can search for any note
+
+print("[" + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "] " + "Fethching notes from the database")
 
 ed_types = ["ED Triage Notes", "ED Provider Notes", "ED Notes"]
 vomit = ["%vomit%", "%barf%", "%puke%", "%puking%", "%emesis%", "%vx%"]
@@ -62,10 +75,14 @@ lemmas = ["vomit", "barf", "puke", "vx", "emesis", "headache", "migraine", "head
 
 # parse each sentence and return all the cols and sentences and problems per sentence and final problems in the note
 
-i = 0
+print("[" + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "] " + "Parsing notes")
+
+# it takes a long time to process these and the gpu queue in hpf is even harder to get in
+# this is a crude pagination so I can resume where I left off it the script does not end overnight
+i = args.skip_n
 nnotes = len(notes)
 parsed_notes = []
-for note_id, visit_id, note_type, author_type, author_service, note_text in notes:
+for note_id, visit_id, note_type, author_type, author_service, note_text in notes[i:]:
     parsed_note = {"note_id": note_id,
                    "visit_id": visit_id,
                    "note_type": note_type,
@@ -86,13 +103,15 @@ for note_id, visit_id, note_type, author_type, author_service, note_text in note
         if len(sent_problems) > 0:
             sent_dict = {"sentence": problem_sent, "problems": list(set(sent_problems))}
             parsed_note["relevant_sentences"].append(sent_dict)
+        note_problems=note_problems+sent_problems
     parsed_note["note_problems"] = list(set(note_problems))
     parsed_notes.append(parsed_note)
 
     i += 1
-    if i % 100 == 0:
-        print("{} out of {} notes processed".format(i, nnotes))
+    if i % 1000 == 0:
+        print("[" + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "] " + "{} out of {} notes processed".format(i, nnotes))
+        
 
 parsed_df = pd.DataFrame(parsed_notes)
-
 parsed_df.to_json("parsed_ed_notes.json", orient="records")
+
